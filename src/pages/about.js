2602ff9df90f24faf6c723a6cc9688dda04ba33e
Andric Tham <andric@nuisance.co>
2017-10-07 20:46:53 +0800

    Abstract site title out to `siteMatadata`, and access it from props.data.site.siteMetadata

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default () =>
+export default (props) =>
   <div>
-    <h1>About Pandas Eating Lots</h1>
+    <h1>About {props.data.site.siteMetadata.title}</h1>
     <p>
       We’re the only site running on your computer dedicated to  showing phoos and videos of pandas eating lots of food.
     </p>
   </div>
+
+export const query = graphql`
+  query AboutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
