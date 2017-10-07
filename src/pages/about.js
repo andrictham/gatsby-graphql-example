c9b265927bb0811cf4797e95b104182400bc65f0
Andric Tham <andric@nuisance.co>
2017-10-07 20:36:35 +0800

    Create pages

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () =>
+  <div>
+    <h1>About Pandas Eating Lots</h1>
+    <p>
+      We’re the only site running on your computer dedicated to  showing phoos and videos of pandas eating lots of food.
+    </p>
+  </div>
