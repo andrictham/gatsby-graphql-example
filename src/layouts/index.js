55663caefc3622012e5b034e83cb61ee35b61c48
Andric Tham <andric@nuisance.co>
2017-10-08 16:57:02 +0800

    Add /my-files to navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,29 +5,49 @@ import Link from 'gatsby-link'
 
 import { rhythm } from '../utils/typography'
 
-const linkStyle = css({ float: `right` })
+const linkStyle = css({
+  marginRight: `1rem`
+})
 
 export default ({ children, data }) =>
-  <g.Div
-    margin={`0 auto`}
-    maxWidth={700}
-    padding={rhythm(2)}
-    paddingTop={rhythm(1.5)}
-  >
-    <Link to={'/'}>
-      <g.H3
-        marginBottom={rhythm(2)}
-        display={`inline-block`}
-        fontStyle={`normal`}
-      >
-        {data.site.siteMetadata.title}
-      </g.H3>
-    </Link>
-    <Link className={linkStyle} to={`/about`}>
-      About
-    </Link>
-    {children()}
-  </g.Div>
+  <div>
+    <g.Div
+      margin={`0 auto`}
+      maxWidth={700}
+      padding={rhythm(2)}
+      paddingTop={rhythm(1.5)}
+      paddingBottom={rhythm(1)}
+      display={`flex`}
+      flexDirection={`row`}
+      justifyContent={`space-between`}
+    >
+      <Link to={'/'}>
+        <g.H3
+          marginBottom={rhythm(2)}
+          display={`inline-block`}
+          fontStyle={`normal`}
+        >
+          {data.site.siteMetadata.title}
+        </g.H3>
+      </Link>
+      <div>
+        <Link className={linkStyle} to={`/about`}>
+          About
+        </Link>
+        <Link className={linkStyle} to={`/my-files`}>
+          Files
+        </Link>
+      </div>
+    </g.Div>
+    <g.Div
+      margin={`0 auto`}
+      maxWidth={700}
+      padding={rhythm(2)}
+      paddingTop={0}
+    >
+      {children()}
+    </g.Div>
+  </div>
 
 export const query = graphql`
   query LayoutQuery {
