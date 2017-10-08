f3cf5b957022444778e6a7ce8f14b2e4ccd371da
Andric Tham <andric@nuisance.co>
2017-10-08 17:38:10 +0800

    Query and display our blog posts in index.js!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,48 @@
 import React from "react"
+import g from 'glamorous'
+import Pluralize from 'react-pluralize'
 
-export default () =>
+import { rhythm } from '../utils/typography'
+
+export default ({ data }) =>
   <div>
-    <h1>Amazing Pandas Eating Things!</h1>
-    <div>
-      <img src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-      alt="Group of pandas eating bamboo"/>
-    </div>
+    <g.H1 display={"inline-block"}>
+      Amazing Pandas Eating Things
+    </g.H1>
+    <g.H4 paddingBottom={rhythm(2)}>
+      <Pluralize singular="post" plural="posts" count={data.allMarkdownRemark.totalCount} />
+    </g.H4>
+    {data.allMarkdownRemark.edges.map( ({ node }) =>
+      <g.Div key={node.id} marginBottom={rhythm(1)} borderBottom={"1px solid #DDDDDD"}>
+        <g.H3>
+          {node.frontmatter.title}{" "}
+          <g.Span color="#999999">– {node.frontmatter.date}</g.Span>
+        </g.H3>
+        <g.P>
+          {node.excerpt} <br/>
+        </g.P>
+        <g.P color="#999999">
+          {node.timeToRead} minute read
+        </g.P>
+      </g.Div>
+    )}
   </div>
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+          timeToRead
+        }
+      }
+    }
+  }
+`
