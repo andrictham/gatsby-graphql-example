9a61f12812b8b7a2f6bf7f0cfd7e9808e109d108
Andric Tham <andric@nuisance.co>
2017-10-08 17:42:20 +0800

    Sort posts by date, descending

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,10 @@ export default ({ data }) =>
 
 export const query = graphql`
   query IndexQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: {
+      fields: [frontmatter___date],
+      order: DESC
+    }) {
       totalCount
       edges {
         node {
