b572fc532fbb9c91e4d6471944d462b131a671c9
Andric Tham <andric@nuisance.co>
2017-10-11 22:51:51 +0800

    Update blog post template to query dynamic data

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,28 @@
 import React from 'react'
 
-export default () =>
-  <div>Hello blog post</div>
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <div>
+      <h1>
+        {post.frontmatter.title}
+      </h1>
+      <div dangerouslySetInnerHTML={{__html: post.html}} />
+    </div>
+  )
+}
+
+// http://graphql.org/learn/queries/
+// $slug is a variable that we passed in as context
+// `eq` is a GraphQL operator meaning “equal”
+
+export const query = graphql`
+  query BlogPostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
